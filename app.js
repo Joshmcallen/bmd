d5d353252a42c3905d8f54a1368f7b899fd504d8
joshmcallen <joshmcallen@gmail.com>
2016-11-22 00:12:33 -0600

    add views w/basic routes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 /////////// Congfiguring MongoDB and Monk////////////
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/dbm');
+var db = monk('localhost:27017/bmd');
 ////////////////////////////////////////////////////
 
 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/news', function(req, res, next){
+  res.render('news', {title: 'News'});
+});
+
+router.get('/movies', function(req, res, next) {
+  res.render('movies', {title: 'Movies'});
+});
+
+router.get('/reviews', function(req, res, next){
+  res.render('reviews', {title: "Reviews"});
+});
+
+router.get('/trailers', function(req, res, next){
+  res.render('trailers', {title: 'Trailers'});
+});
+
+router.get('/podcasts', function(req, res, next){
+  res.render('trailers', {title: 'Podcasts'});
+});
+
+router.get('/addpost', function(req, res, next){
+  res.render('addpost', {title: 'Add Post'});
+});
+
+router.get('/adduser', function(req, res, next){
+  res.render('adduser', {title: 'Add User'});
+});
+
+
+
 module.exports = router;
